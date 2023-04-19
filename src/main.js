ee15c982a9b90550f570bcdfa01c09b2715c406a
zhuhaojie <zhuhaojie@cqslim.com>
2023-04-19 20:57:50 +0800

    feat: 提交一下

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+// import { Button } from "@cq/components";
+import '@cq/components/lib/style.css'
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+
+// app.use(Button)
+
+app.mount('#app')
